cb0b9c3f3657559d7f5ef4face9e6866f628d062
ancyra2 <duran.talih@gmail.com>
2021-08-17 11:16:17 +0300

    Ödev istenilen şekilde yapıldı.

--- app.php
+++ app.php
@@ -7,6 +7,39 @@ $age = 25; // yaş (sene)
 
 $guess = "1470"; // Tahmin edilen değer
 
+
+
+if($gender == "male"){ // Cinsiyetin erkek olması durumu
+
+    $male_bmr = 88.362 + (13.397 * $weight) + (4.799 * $height) - (5.677 * $age); // bmr (male)
+
+    echo "BMR: " . $male_bmr ;
+    echo "<br>";
+
+    if ($male_bmr < $guess) {
+        echo "Tahmin değerinden düşük";
+    } elseif ($male_bmr == $guess){
+        echo "Tahmin değerine eşit";
+    } else {
+        echo "Tahmin değerinden büyük";
+    }
+}else{ // Cinsiyetin kadın olması durumu
+
+    $female_bmr = 447.593 + (9.247 * $weight) + (3.098 * $height) - (4.330 * $age); // bmr (female)
+    
+	echo "BMR: " . $female_bmr;
+	echo "<br>";
+
+    if ($female_bmr < $guess) {
+        echo "Tahmin değerinden düşük";
+    } elseif ($female_bmr == $guess){
+        echo "Tahmin değerine eşit";
+    } else {
+        echo "Tahmin değerinden büyük";
+    }
+}
+
+
 /**
  * Bu ödevde bazal metabolizma hesaplaması yapacağız.
  * Yukarıdaki değişkenleri ve aşağıdaki formülü kullanarak
